777945c794bf083847bff2bdbda41e8529ca225b
Marko1212 <shihonage7@gmail.com>
2020-11-25 16:54:49 +0100

    update

---
+++ cours-php/10-upload/index.php
@@ -0,0 +1,30 @@
+<!-- 1. Nous allons créer un formulaire de candidature à une offre d'emploi
+
+2. L'utilisateur pourra envoyer son CV en PDF. On devra vérifier le format et la taille (limité à 5mo).
+
+3. L'utilisateur pourra saisir son email, on renommera le CV avec son email et une chaine de caractères générée aléatoirement.
+
+4. On ajoutera l'upload d'une image. Idéalement, on essaiera de redimensionner l'image en 300x300 dans un nouveau fichier. 
+Voir l'extension GD. -->
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload</title>
+</head>
+
+<body>
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+        <input type="text" name="prenom" placeholder="Prénom">
+        <input type="file" name="file">
+        <input type ="file" name="image">
+        <button type="submit" name="submit">UPLOAD</button>
+    </form>
+   
+</body>
+
+</html>
---
+++ cours-php/10-upload/upload.php
@@ -0,0 +1,117 @@
+<?php
+
+function imageResize($image, $w, $h)
+{
+    $oldw = imagesx($image);
+    $oldh = imagesy($image);
+    $temp = imagecreatetruecolor($w, $h);
+    imagecopyresampled($temp, $image, 0, 0, 0, 0, $w, $h, $oldw, $oldh);
+    return $temp;
+}
+
+
+if (isset($_POST['submit'])) {
+
+    if ($_FILES['file']['size'] === 0 || $_FILES['image']['size'] === 0) {
+        echo "Veuillez sélectionner votre CV au format PDF et une image au format JPEG!";
+    }
+    
+    else {
+
+    $file = $_FILES['file'];
+
+    $prenom = $_POST['prenom'];
+
+    $image = $_FILES['image'];
+
+    //print_r($file);
+
+    $fileName = $_FILES['file']['name'];
+    $fileTmpName = $_FILES['file']['tmp_name'];
+    $fileSize = $_FILES['file']['size'];
+    $fileError = $_FILES['file']['error'];
+    $fileType = $_FILES['file']['type'];
+
+    $fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+
+    //$formatsAutorises = array('jpg', 'jpeg', 'png', 'pdf');
+
+    //in_array($fileActualExt, $formatsAutorises)
+    if ($fileActualExt === "pdf") {
+        if ($fileError === 0) {
+            if ($fileSize <= 5000000) {
+                $fileNameNew = $prenom.'-'.uniqid('', true).".".$fileActualExt;
+                $fileDestination = 'uploads/'.$fileNameNew;
+                move_uploaded_file($fileTmpName, $fileDestination);
+                header("Location:index.php?uploadsuccess");
+            } else {
+                echo "Votre fichier est trop gros!";
+            }
+    } else {
+             echo "Il y a eu une erreur avec l'upload de votre fichier!";
+            }   
+}
+    else {
+            echo "Vous ne pouvez pas uploader des fichiers de ce type!";
+         }
+
+
+        $imageName = $_FILES['image']['name'];
+        $imageTmpName = $_FILES['image']['tmp_name'];
+        $imageSize = $_FILES['image']['size'];
+        $imageError = $_FILES['image']['error'];
+        $imageType = $_FILES['image']['type'];
+    
+        $imageExt = explode('.', $imageName);
+        $imageActualExt = strtolower(end($imageExt));
+
+        if ($imageActualExt === "jpg" || $imageActualExt === "jpeg") {
+            if ($imageError === 0) {
+                if ($imageSize <= 5000000) {
+                    $imageNameNew = uniqid('', true).".".$imageActualExt;
+                    $imageDestination = 'uploads/'.$imageNameNew;
+                    move_uploaded_file($imageTmpName, $imageDestination);
+                    
+
+                    $uploadedImage = imagecreatefromjpeg($imageDestination);
+
+                    $resizedImage = imageResize($uploadedImage,300,300);
+                    $imageNameNew = '300x300-'.$imageNameNew;
+                    $imageDestinationNew = 'uploads/'.$imageNameNew;
+                            
+                    // enregistrer l'image sur le disque
+                    imagejpeg($resizedImage, $imageDestinationNew);
+        
+                         
+
+
+
+
+
+                    header("Location:index.php?uploadsuccess");
+
+
+                }
+                
+                else {
+                    echo "Votre fichier est trop gros!";
+                    }
+          }  
+          
+                else {
+                 echo "Il y a eu une erreur avec l'upload de votre fichier!";
+                     }   
+    }
+        else {
+                echo "Vous ne pouvez pas uploader des fichiers de ce type!";
+             }
+    
+        
+            }
+
+    }
+
+
+
+?>
