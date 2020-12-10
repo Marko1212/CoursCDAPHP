6f9a819df76723a9a142ce5eebf953a5aad9bb99
Marko1212 <shihonage7@gmail.com>
2020-12-10 13:56:07 +0100

    change

--- cours-php/15-webflix/public/assets/js/script.js
+++ cours-php/15-webflix/public/assets/js/script.js
@@ -4,3 +4,17 @@
 if ($(window).height() >= $(document).height()) {
     $('footer').addClass('sticky-to-bottom');
 }
+
+
+
+ //on doit exécuter le code précédent au resize de la fenêtre
+
+$(window).resize(function() {
+
+    if ($(window).height() >= $(document).height()) {
+        $('footer').addClass('sticky-to-bottom');
+    } else {
+        $('footer').removeClass('sticky-to-bottom');
+    }
+
+});
