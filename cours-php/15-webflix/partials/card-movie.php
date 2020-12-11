a79bb5b9e9a97c632289962ee8d4b20199e4255c
Marko1212 <shihonage7@gmail.com>
2020-12-11 11:01:32 +0100

    update

--- cours-php/15-webflix/partials/card-movie.php
+++ cours-php/15-webflix/partials/card-movie.php
@@ -5,23 +5,44 @@
             <h5 class="card-title"><?= $movie['title']; ?></h5>
             <p class="card-text">Sorti en <?= substr($movie['released_at'], 0, 4); ?></p>
             <p class="card-text"><?= truncate($movie['description']); ?></p>
-            <a href="movie_single.php?id=<?=$movie['id']; ?>" class="btn btn-danger btn-block">Voir le film</a>
+            <a href="movie_single.php?id=<?= $movie['id']; ?>" class="btn btn-danger btn-block">Voir le film</a>
             <?php if (isAdmin()) { ?>
-            <a href="movie_update.php?id=<?=$movie['id']; ?>" class="btn btn-secondary btn-block">Modifier le film</a>
-            <a href="movie_delete.php?id=<?=$movie['id']; ?>" class="btn btn-secondary btn-block">Supprimer le film</a>
+                <a href="movie_update.php?id=<?= $movie['id']; ?>" class="btn btn-secondary btn-block">Modifier le film</a>
+                <a href="movie_delete.php?id=<?= $movie['id']; ?>" class="btn btn-secondary btn-block" onclick="/*return confirm('Voulez vous supprimer le film?');*/" data-toggle="modal" data-target="#deleteModal<?= $movie['id']; ?>">Supprimer le film</a>
+                <!-- Modal -->
+                <div class="modal fade" id="deleteModal<?= $movie['id']; ?>" tabindex="-1">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Supprimer <?= $movie['title']; ?></h5>
+                                <button type="button" class="close" data-dismiss="modal">
+                                    <span>&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                Êtes-vous sûr(e) de vouloir supprimer le film?
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Non</button>
+                                <a class="btn btn-danger" href="movie_delete.php?id=<?= $movie['id']; ?>">Oui</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             <?php } ?>
         </div>
         <div class="card-footer text-muted">
-        <?php 
-                   $averageMovie = getAverageMovie($movie['id']); 
-                   echo $averageMovie.'/5'; 
-                   
-                   //Boucle pour afficher les étoiles
+            <?php
+            $averageMovie = getAverageMovie($movie['id']);
+            echo $averageMovie . '/5';
 
-                   for ($i = 1; $i <= 5; $i++) {
-                        echo ($i <= $averageMovie) ? '★' : '☆';
-                   }
-        ?>
+            //Boucle pour afficher les étoiles
+
+            for ($i = 1; $i <= 5; $i++) {
+                echo ($i <= $averageMovie) ? '★' : '☆';
+            }
+            ?>
         </div>
     </div>
 </div>
