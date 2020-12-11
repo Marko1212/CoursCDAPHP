51e9d89b4a9c2f5ed1a4111799b28d4828979a3c
Marko1212 <shihonage7@gmail.com>
2020-12-11 09:51:47 +0100

    update

--- cours-php/15-webflix/public/movie_delete.php
+++ cours-php/15-webflix/public/movie_delete.php
@@ -1,5 +1,7 @@
 <?php
 
+ob_start();
+
 //on inclut le header (et le footer via les fonctions display403 et display404) parce qu'on a besoin d'afficher une page si l'utilisateur n'est pas autorisé à effacer le film; aussi, c'est via le header qu'on accède aux fonctions
 require '../partials/header.php';
 
