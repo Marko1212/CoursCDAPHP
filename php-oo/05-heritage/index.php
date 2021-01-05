edd5d79eb8528ba53abdc1438ed1489703e7af32
Marko1212 <shihonage7@gmail.com>
2021-01-05 18:04:54 +0300

    update

--- php-oo/05-heritage/index.php
+++ php-oo/05-heritage/index.php
@@ -45,9 +45,82 @@
 
     echo 'Un chat a normalement '.Chat::PAW.' pattes <br>';
 
-    var_dump($catA);
-    var_dump($catB);
+    //var_dump($catA);
+    //var_dump($catB);
 
    
+    /**
+     * L'héritage en POO
+     */
+
+    class Animal {
+        protected $name;
+        protected $type;
+
+        public function __construct($name) {
+            $this->name = $name;
+        }
+
+        public function move() {
+            return $this->name. ' se déplace';
+        }
+
+    }
+
+    // La classe Cat hérite du comportement de la classe Animal
+    // On peut préfixer la classe avec le mot clé final pour éviter d'hériter de Cat (final class Cat)
+    class Cat extends Animal {
+        private $fur;
+
+        public function climbsOnRoof() {
+            return $this->name. ' est sur le toit...<br>';
+        }
+
+        public function move() {
+            // parent::move() permet d'appliquer la méthode move du parent (Animal)
+            //On peut aussi faire $this->move() dans une méthode qui n'a pas le nom move
+            return parent::move().' silencieusement... <br>';
+        }
+
+
+    }
+
+    $bianca = new Cat('Bianca');//ici on utilise le constructeur de la classe Animal
+
+    var_dump($bianca);
+    echo $bianca->move();
+    echo $bianca->climbsOnRoof();
+
+    // Avec l'héritage, on a ce qu'on appelle le polymorphisme
+
+    var_dump($bianca instanceof Animal);
+    var_dump($bianca instanceof Cat);
+
+    echo '<br>';
+
+    class Dog extends Animal {
+
+        public function __construct($name, $type) {
+            parent::__construct($name);
+            $this->type = $type;
+        }        
+
+        public function move() {
+            // parent::move() permet d'appliquer la méthode move du parent (Animal)
+            //On peut aussi faire $this->move() dans une méthode qui n'a pas le nom move
+            return parent::move().' bruyamment... <br>';
+        }
+
+        public function takeAWalk() {
+            return $this->name. ' se promène avec son maître...<br>';
+        }
+
+    }
+
+    $dog = new Dog('Pongo', 'Dalmatien');
+    echo $dog->move();
+    echo $dog->takeAWalk();
+    var_dump($dog);
+
 
 
