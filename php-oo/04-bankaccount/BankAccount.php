d18801e461c80ea5a1f9a9743b40d37d58807e92
Marko1212 <shihonage7@gmail.com>
2021-01-05 08:28:26 +0300

    update

--- php-oo/04-bankaccount/BankAccount.php
+++ php-oo/04-bankaccount/BankAccount.php
@@ -1,13 +1,17 @@
-<?php 
+<?php
 
-class BankAccount {
+class BankAccount
+{
 
-private $number;
-private static $id=1;
-private $owner;
-private $balance;
-private $overdraft;
+    private $number;
+    //private static $id=1;
+    private $owner;
+    private $balance;
+    private $overdraft;
 
+    private static $accountNumbers = [];
+
+    /* génération des comptes bancaires distincts
 
 public function __construct($owner, $balance=0, $overdraft=0) {
     $this->number = self::$id;
@@ -17,61 +21,80 @@ public function __construct($owner, $balance=0, $overdraft=0) {
     $this->overdraft=$overdraft;
 }
 
+*/
 
-public function getNumber() {
-    return $this->number;
-}
+    public function __construct($owner, $balance = 0, $overdraft = 0)
+    {
 
-public function getOwner() {
-    return $this->owner;
-}
+        $this->number = mt_rand(100000, 999999);
 
-public function getBalance() {
-    return $this->balance;
-}
-public function depositMoney($credit) {
+        while (in_array($this->number, self::$accountNumbers)) {
+            $this->number = mt_rand(100000, 999999);
+        }
+        
+        self::$accountNumbers[] = $this->number;
 
-    if ($credit < 0) {
-        echo 'You must deposit positive amount of money<br>';
-        return;
-    }
-    
-    $this->balance += $credit;
+        $this->owner = $owner;
+        $this->balance = $balance;
+        $this->overdraft = $overdraft;
 
-    return $this;
-
-}
-
-public function withdrawMoney($debit) {
-    
-    if ($debit <= 0) {
-        echo 'You must withdraw positive amount of money<br>';
-        return;
-    }
-    if ($this->balance - $debit < 0 - $this->overdraft) {
-    echo 'You don\'t have enough money for this transaction<br>';
-    return;
     }
 
-    $this->balance -= $debit;
 
-    return $this;
 
-}
-
-public function applyFees() {
-    
-    if ($this->balance < 0 ) {
-    $this->balance = -$this->balance;
-    $this->balance += 0.1*$this->balance;
-    $this->balance = -$this->balance;
+    public function getNumber()
+    {
+        return $this->number;
     }
 
-    return $this;
+    public function getOwner()
+    {
+        return $this->owner;
+    }
 
-}
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+    public function depositMoney($credit)
+    {
 
+        if ($credit < 0) {
+            echo 'You must deposit positive amount of money<br>';
+            return;
+        }
 
+        $this->balance += $credit;
 
+        return $this;
+    }
 
+    public function withdrawMoney($debit)
+    {
+
+        if ($debit <= 0) {
+            echo 'You must withdraw positive amount of money<br>';
+            return;
+        }
+        if ($this->balance - $debit < 0 - $this->overdraft) {
+            echo 'You don\'t have enough money for this transaction<br>';
+            return;
+        }
+
+        $this->balance -= $debit;
+
+        return $this;
+    }
+
+    public function applyFees()
+    {
+
+        if ($this->balance < 0) {
+            $this->balance = -$this->balance;
+            $this->balance += 0.1 * $this->balance;
+            $this->balance = -$this->balance;
+        }
+
+        return $this;
+    }
 }
