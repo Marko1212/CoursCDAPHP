bf72fd1a6c50db33d84e6e8b1888771cb8fc7c25
Marko1212 <shihonage7@gmail.com>
2021-01-11 13:16:54 +0300

    update

---
+++ php-oo/09-form/Form.php
@@ -0,0 +1,22 @@
+<?php
+
+class Form {
+   private $fields = [];
+
+    
+    /**
+     * Permet de définir un champ dans mon formulaire
+     */
+
+    public function input($name) {
+        $this->fields[] = $name;
+        $label = ucfirst($name);
+
+        return "<label for=\$name\">$label</label>
+                <input type=\"text\" id=\"$name\"
+                class=\"form-control\">";
+
+    }
+
+}
+
---
+++ php-oo/09-form/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire en POO</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <?php
+    spl_autoload_register();
+
+
+    $form = new Form();
+
+
+
+    ?>
+
+    <form method='post' action=''>
+        <?php echo $form->input('email'); ?>
+        <?php echo $form->input('telephone'); ?>
+        <?php echo $form->input('message'); ?>
+    </form>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
