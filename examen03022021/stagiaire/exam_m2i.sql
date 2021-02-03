74f48dfffea9c4ef80b3ad666a10189bec7c11f1
Marko1212 <shihonage7@gmail.com>
2021-02-03 16:07:56 +0100

    update

--- examen03022021/stagiaire/exam_m2i.sql
+++ examen03022021/stagiaire/exam_m2i.sql
@@ -1,4 +1,4 @@
--- Adminer 4.7.8 MySQL dump
+
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -21,4 +21,3 @@ INSERT INTO `stagiaire` (`id`, `created_at`, `name`, `phone`, `birthday`) VALUES
 (1,	'2021-02-03 15:51:19',	'Olga',	'1234567890',	'2021-04-14'),
 (2,	'2018-04-03 15:53:29',	'Andjelija',	'0987654321',	'1999-03-04');
 
--- 2021-02-03 13:18:31
