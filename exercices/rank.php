c872562386060f2cfbba3976102f53340e904034
Marko1212 <shihonage7@gmail.com>
2021-06-10 07:07:47 +0200

    update

--- exercices/rank.php
+++ exercices/rank.php
@@ -5,8 +5,11 @@ $values = array();
 $values[0] = 17;
 $values[1] = 27;
 $values[2] = 17;
-$values[3] = 5;
-$values[4] = 3;
+$values[3] = '-';
+$values[4] = 5;
+$values[5] = 3;
+$values[6] = '-';
+
 
 $ordered_values = $values;
 rsort($ordered_values);
