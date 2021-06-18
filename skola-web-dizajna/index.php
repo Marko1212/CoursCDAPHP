cb104c2f5f9e746c2051eeb32060b96e4e9de6f5
Marko1212 <shihonage7@gmail.com>
2021-06-18 22:50:20 +0200

    first

---
+++ skola-web-dizajna/index.php
@@ -0,0 +1,30 @@
+<?php include "header.php"; ?>
+
+    <!-- header -->
+    <header class="mainHeader py-5">
+        <article class="container d-flex justify-content-center flex-column text-white text-center">
+            <div class="row">
+                <div class="col-md-6 mx-auto text-center">
+                    <h1>Škola veb dizajna </h1>
+                    <p class="lead">će vam pomoći da za najkraće vreme postanete <br> veb dizajner, front end developer ili WordPress developer </p>
+                </div>
+            </div>
+            <div class="row justify-content-between">
+                <div class="col-md-4 p-4 rounded-lg">
+                    <h3>HTML i CSS</h3>
+                    <a href="">Saznaj vise</a>
+                </div>
+                <div class="col-md-4 p-4 rounded-lg">
+                    <h3>WordPress</h3>
+                    <a href="">Saznaj vise</a>
+                </div>
+                <div class="col-md-4 p-4 rounded-lg">
+                    <h3>Javascript i PHP</h3>
+                    <a href="">Saznaj vise</a>
+                </div>
+            </div>
+        </article>
+    </header>
+
+<?php include "footer.php"; ?>
+    
