860701bf10c35a464ee7ed771cdeadd1df0bcd98
Marko1212 <shihonage7@gmail.com>
2021-09-24 10:18:58 +0200

    exo

--- exercices/rank.php
+++ exercices/rank.php
@@ -1,7 +1,7 @@
 
 <?php
 
-$values = array();
+$values = [];
 $values[0] = 17;
 $values[1] = 27;
 $values[2] = 17;
@@ -10,9 +10,24 @@ $values[4] = 5;
 $values[5] = 3;
 $values[6] = '-';
 
+$values2 = [];
+$values2['zero'] = 17;
+$values2['one'] = 27;
+$values2['two'] = 17;
+$values2['three'] = '-';
+$values2['four'] = 5;
+$values2['five'] = 3;
+$values2['six'] = '-';
 
 $ordered_values = $values;
 rsort($ordered_values);
+
+$ordered_values2 = $values2;
+rsort($ordered_values2);
+
+foreach ($ordered_values2 as $key => $value) {
+    echo 'key : '.$key.' '.' value : '.$value.'<br/>';
+}
 
 foreach ($values as $key => $value) {
     foreach ($ordered_values as $ordered_key => $ordered_value) {
@@ -21,5 +36,5 @@ foreach ($values as $key => $value) {
             break;
         }
     }
-    echo $value . '- Rank: ' . ((int) $key + 1) . '<br/>';
+    echo $value.'- Rank: '.((int) $key + 1).'<br/>';
 }
