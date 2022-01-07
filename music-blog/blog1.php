62aa358c86eb54feafc1ee7711f9ec67fd310669
Marko1212 <shihonage7@gmail.com>
2022-01-07 21:55:09 +0100

    correction

--- music-blog/blog1.php
+++ music-blog/blog1.php
@@ -1,109 +1,67 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include('header.php'); ?>
 
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Music blog</title>
-    <link rel="stylesheet" href="style.css" />
-</head>
+<!-- header -->
+<header class="mainHeader">
+    <img src="img/blog1.jpg" alt="" />
+    <h1>Blog 1</h1>
+</header>
+<!-- main content -->
 
-<body>
-    <!-- nav -->
-    <nav>
-        <a href="index.php" class="logo">Pink music</a>
-        <ul>
-            <li><a href="index.php">Naslovna</a></li>
-            <li><a href="">O nama</a></li>
-            <li><a href="">Kontakt</a></li>
-        </ul>
-    </nav>
+<main class="container">
+    <section>
+        <article>
+            <h2>Blog 1</h2>
+            <p class="meta">Admin | 3.01.2022.</p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Earum tempore et dolor aliquam explicabo aperiam tenetur
+                aliquid rerum cumque necessitatibus!
+            </p>
+            <br /><br />
+            <hr />
+            <br /><br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Similique nisi officiis error beatae? Voluptates
+                eligendi quos saepe, suscipit consequatur ipsum, soluta
+                necessitatibus tempora consectetur aut, tempore
+                asperiores voluptatibus? Ex ea numquam molestias atque,
+                porro quae delectus veniam dolorem, ipsam iusto
+                consequatur dolore eum ratione eaque a error corporis?
+                Quidem eaque illum quibusdam quod commodi impedit
+                dolorum culpa est fugit facere iste inventore illo
+                ratione fugiat quaerat, ea quae numquam iusto officiis
+                minus quam nobis sapiente provident hic. Aliquid numquam
+                facere corporis neque doloremque magni sint porro ab,
+                modi fuga dolorum ut earum impedit vel a dolor deleniti
+                nesciunt. Praesentium, officiis!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Pariatur earum tempore quasi quis adipisci, repudiandae
+                neque perspiciatis rem architecto corrupti sit illo
+                quisquam voluptas ratione consequuntur ex eveniet
+                delectus dolorem omnis vitae quibusdam quia consectetur
+                voluptatum. Doloremque odio at velit fugiat ex eveniet
+                sit veritatis qui illo. Expedita ratione fugiat corrupti
+                quod voluptatem. Repellendus ducimus qui, dolorum, iusto
+                animi veritatis vel beatae harum dignissimos vero cum
+                laboriosam rem rerum atque corrupti molestiae delectus
+                minus debitis sequi quos nisi. Asperiores quos totam
+                voluptas in. Ad, in. Placeat veritatis perferendis at
+                voluptas fugit repellendus sunt voluptatum ullam, animi
+                iure natus? Natus doloribus sint animi explicabo maxime
+                hic reiciendis omnis, aliquam tenetur quidem, a sunt
+                illo. Asperiores dolor quis doloremque quidem sapiente
+                maxime voluptas recusandae repellat. Blanditiis
+                molestiae mollitia tenetur, optio, debitis placeat quis
+                laborum quae tempore velit ullam minus totam
+                reprehenderit delectus accusantium cupiditate enim error
+                nihil alias unde dignissimos quam voluptates.
+            </p>
+        </article>
+    </section>
+    <?php include('sidebar.php'); ?>
+</main>
 
-    <!-- header -->
-    <header class="mainHeader">
-        <img src="img/blog1.jpg" alt="" />
-        <h1>Blog 1</h1>
-    </header>
-    <!-- main content -->
-
-    <main class="container">
-        <section>
-            <article>
-                <h2>Blog 1</h2>
-                <p class="meta">Admin | 3.01.2022.</p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Earum tempore et dolor aliquam explicabo aperiam tenetur
-                    aliquid rerum cumque necessitatibus!
-                </p>
-                <br /><br />
-                <hr />
-                <br /><br />
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Similique nisi officiis error beatae? Voluptates
-                    eligendi quos saepe, suscipit consequatur ipsum, soluta
-                    necessitatibus tempora consectetur aut, tempore
-                    asperiores voluptatibus? Ex ea numquam molestias atque,
-                    porro quae delectus veniam dolorem, ipsam iusto
-                    consequatur dolore eum ratione eaque a error corporis?
-                    Quidem eaque illum quibusdam quod commodi impedit
-                    dolorum culpa est fugit facere iste inventore illo
-                    ratione fugiat quaerat, ea quae numquam iusto officiis
-                    minus quam nobis sapiente provident hic. Aliquid numquam
-                    facere corporis neque doloremque magni sint porro ab,
-                    modi fuga dolorum ut earum impedit vel a dolor deleniti
-                    nesciunt. Praesentium, officiis!
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Pariatur earum tempore quasi quis adipisci, repudiandae
-                    neque perspiciatis rem architecto corrupti sit illo
-                    quisquam voluptas ratione consequuntur ex eveniet
-                    delectus dolorem omnis vitae quibusdam quia consectetur
-                    voluptatum. Doloremque odio at velit fugiat ex eveniet
-                    sit veritatis qui illo. Expedita ratione fugiat corrupti
-                    quod voluptatem. Repellendus ducimus qui, dolorum, iusto
-                    animi veritatis vel beatae harum dignissimos vero cum
-                    laboriosam rem rerum atque corrupti molestiae delectus
-                    minus debitis sequi quos nisi. Asperiores quos totam
-                    voluptas in. Ad, in. Placeat veritatis perferendis at
-                    voluptas fugit repellendus sunt voluptatum ullam, animi
-                    iure natus? Natus doloribus sint animi explicabo maxime
-                    hic reiciendis omnis, aliquam tenetur quidem, a sunt
-                    illo. Asperiores dolor quis doloremque quidem sapiente
-                    maxime voluptas recusandae repellat. Blanditiis
-                    molestiae mollitia tenetur, optio, debitis placeat quis
-                    laborum quae tempore velit ullam minus totam
-                    reprehenderit delectus accusantium cupiditate enim error
-                    nihil alias unde dignissimos quam voluptates.
-                </p>
-            </article>
-        </section>
-        <aside>
-            <h3>Kategorije</h3>
-            <ul>
-                <li><a href="">Blues</a></li>
-                <li><a href="">Funky</a></li>
-                <li><a href="">Electronic</a></li>
-                <li><a href="">Hip Hop</a></li>
-                <li><a href="">Jazz</a></li>
-                <li><a href="">Heavy metal</a></li>
-            </ul>
-            <h3>Tagovi</h3>
-            <ul>
-                <li><a href="">Domaca</a></li>
-                <li><a href="">Strana</a></li>
-                <li><a href="">Etno</a></li>
-            </ul>
-        </aside>
-    </main>
-
-    <!-- footer -->
-    <footer>
-        <p>&copy; 2022. Websites Workshop</p>
-    </footer>
-</body>
-
-</html>
+<?php include('footer.php'); ?>
